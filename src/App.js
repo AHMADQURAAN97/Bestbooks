e070b30d1c84fecb1196554046aa36dde7aa092c
AHMADQURAAN97 <ahmadqouraan@gmail.com>
2021-08-22 16:48:25 +0300

    complete lab11

--- src/App.js
+++ src/App.js
@@ -8,27 +8,43 @@ import {
   Route
 } from "react-router-dom";
 
+import Profile from "./Profile"
+import BestBooks from "./BestBooks"
+import Login from "./Login"
+import { withAuth0 } from '@auth0/auth0-react';
+
+
+
 class App extends React.Component {
 
   render() {
+    const {isAuthenticated} = this.props.auth0;
+
     console.log('app', this.props);
     return(
       <>
         <Router>
-          <IsLoadingAndError>
+          
             <Header />
             <Switch>
               <Route exact path="/">
                 {/* TODO: if the user is logged in, render the `BestBooks` component, if they are not, render the `Login` component */}
+
+                  
+                { (isAuthenticated) ? <BestBooks/> : <Login/>
+                }
               </Route>
+              <Route exact path="/profile">
+                <Profile/>
+                </Route>
               {/* TODO: add a route with a path of '/profile' that renders a `Profile` component */}
             </Switch>
             <Footer />
-          </IsLoadingAndError>
+          
         </Router>
       </>
     );
   }
 }
 
-export default App;
+export default  withAuth0(App);
--- src/Header.js
+++ src/Header.js
@@ -3,18 +3,33 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from 'react-bootstrap/Navbar';
 import { Link } from "react-router-dom";
 import './Header.css';
+import { withAuth0 } from '@auth0/auth0-react';
+import LogoutButton from './LogoutButton';
+import LoginButton from './LoginButton';
 
 class Header extends React.Component {
   render() {
+
+    const {isAuthenticated} = this.props.auth0;
     return(
+      <div>
       <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
         <Navbar.Brand>My Favorite Books</Navbar.Brand>
         <Link to="/">Home</Link>
         <Link to="/profile">Profile</Link>
         {/* TODO: if the user is logged in, render the `LogoutButton` - if the user is logged out, render the `LoginButton` */}
+         
+      
+         { (isAuthenticated)  ? <LogoutButton/> : <LoginButton/> }
       </Navbar>
+      </div>
     );
   }
 }
 
-export default Header;
+export default withAuth0(Header);
+{/* if ({this.props.auth0.isAuthenticated} === True) {
+          <LogoutButton/>
+        }else {
+          <LoginButton/>
+        } */}
