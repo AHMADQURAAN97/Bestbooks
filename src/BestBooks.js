fc0c08c09b35026d8f7941c18abecaedbb50f483
AHMADQURAAN97 <ahmadqouraan@gmail.com>
2021-08-26 17:20:41 +0300

    complete lab13

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -1,148 +1,144 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './BestBooks.css';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./BestBooks.css";
 import axios from "axios";
-import Card from 'react-bootstrap/Card';
-import Jumbotron from 'react-bootstrap/Jumbotron';
-import { withAuth0 } from '@auth0/auth0-react';
+import Card from "react-bootstrap/Card";
+import Jumbotron from "react-bootstrap/Jumbotron";
+import { withAuth0 } from "@auth0/auth0-react";
 import { Form, Button, ThemeProvider } from "react-bootstrap/";
 
 class MyFavoriteBooks extends React.Component {
-
-
-constructor(props){
-  super(props);
-  this.state = ({
-    book:[],
-  })
-}
-
-// ====================================== GetBook Function=====================
-
-componentDidMount = async ()=>{
-  const { user } = this.props.auth0;
-
-  console.log(this.props.auth0);
-  let emailaddress = user.email;
-  console.log('email',emailaddress);
-
-
-  // http://localhost:3001/books?email=AhmadQouraan@gmail.com
-let booksData = await axios.get(`${process.env.REACT_APP_DATABASE}/books?email=${emailaddress}`);
-console.log(booksData.data);
-
-await this.setState({
-  book:booksData.data,
-})
-
-};
-
-// ========================================== addBook function ============================
-
-addbook = async (e)=> {
-
-  e.preventDefault();
-  const { user } = this.props.auth0;
-
-  let bookInfo = {
-    email:user.email,
-   title: e.target.title.value,
-   description: e.target.description.value,
-   status:" "
+  constructor(props) {
+    super(props);
+    this.state = {
+      book: [],
+    };
   }
 
-let bookInfoData = await axios.post(`${process.env.REACT_APP_DATABASE}/addbook`,bookInfo)
-console.log("boooookInfo",bookInfoData);
+  // ====================================== GetBook Function=====================
 
-  await this.setState ({
-  book:bookInfoData.data ,
-})
+  componentDidMount = async () => {
+    const { user } = this.props.auth0;
 
+    console.log(this.props.auth0);
+    let emailaddress = user.email;
+    console.log("email", emailaddress);
 
+    // http://localhost:3001/books?email=AhmadQouraan@gmail.com
+    let booksData = await axios.get(
+      `${process.env.REACT_APP_DATABASE}/books?email=${emailaddress}`
+    );
+    console.log(booksData.data);
 
-};
-
-// ======================================Delete Function==================
-
-deleteBook = async (id) => {
-  const { user } = this.props.auth0;
-
-  let paramsObj = {
-    email: user.email,
+     this.setState({
+      book: booksData.data,
+    });
   };
 
-  let resData = await axios.delete(`${process.env.REACT_APP_DATABASE}/deletebook/${id}`, { params: paramsObj });
+  // ========================================== addBook function ============================
+
+  addbook = async (e) => {
+    e.preventDefault();
+    const { user } = this.props.auth0;
+
+    let bookInfo = {
+      email: user.email,
+      title: e.target.title.value,
+      description: e.target.description.value,
+    };
+
+    let bookInfoData = await axios.post(
+      `${process.env.REACT_APP_DATABASE}/addbook`,
+      bookInfo
+    );
+    console.log("boooookInfo", bookInfoData);
 
 
-  this.setState ({
-    book:resData.data,
-  })
+ 
+     this.setState({
+      book: [...this.state.book , bookInfoData.data]
+    });
+  };
+
+  // ======================================Delete Function==================
+
+   deleteBook = async (bookID) => {
+    const { user } = this.props.auth0;
+
+   
+
+    let resData = await axios.delete(
+      `${process.env.REACT_APP_DATABASE}/deletebook/${bookID}?email=${user.email}`
+    );
+
+    this.setState({
+      book: resData.data,
+     
+    });
+    
+  };
+ 
+  render() {
+    
+
+    return (
+      <>
+        <div>
+          <Form onSubmit={this.addbook}>
+            <Form.Group>
+              <Form.Control
+                className="mb-2"
+                type="text"
+                placeholder="Book name"
+                name="title"
+              />
+
+              <Form.Control
+                className="mb-2"
+                type="text"
+                placeholder="Book description"
+                name="description"
+              />
+              <input type="submit" value="Add Book" />
+            </Form.Group>
+          </Form>
+        </div>
+        <Jumbotron>
+          <h1> My Favorite Books</h1>
+          <p>This is a collection of my favorite books</p>
+        </Jumbotron>
+
+        <div>
+          {this.state.book.length !== 0 ? (
+            this.state.book.map((item, i) => {
+              return (
+                <li key={i}>
+                  <h4> {item.title}</h4>
+                  <p>{item.description}</p>
+                  <p>{item.email}</p>
+                  <Button
+                    variant="bottom"
+                    variant="danger"
+                    onClick={() => this.deleteBook(item._id)}
+                  >
+                    Delete
+                  </Button>
+                </li>
+              );
+            })
+          ) : (
+            <p> Not exist any Book </p>
+          )}
+        </div>
+      </>
+    );
+  }
 }
 
-
-
-
-
-  render() {
-    console.log(this.state.book);
-
-    return(
-      <>
-    <div>
-    <Form  onSubmit={this.addbook}>
-          <Form.Group>
-            <Form.Control
-              className="mb-2"
-              type="text"
-              placeholder="Book name"
-              name="title"
-            />
-
-            <Form.Control
-              className="mb-2"
-              type="text"
-              placeholder="Book description"
-              name="description"
-            />
-            <input type="submit" value="Add Book" />
-          </Form.Group>
-          
-        </Form>
-
-
-    </div>
-      <Jumbotron>
-        <h1> My Favorite Books</h1>
-        <p>
-        This is a collection of my favorite books
-        </p>
-      </Jumbotron>
-
-      <div>
-       
-      {this.state.book.length !== 0 && this.state.book.map((item,i) => { 
-
-return (
-
-  <li key={i}>
-  <h4> {item.title}</h4>
-  <p>{item.description}</p>
-  <p>{item.email}</p>
-  <Button variant="bottom" variant="danger" onClick={() => this.deleteBook(i)}>
-  Delete 
-        </Button>
- 
-</li>
-      )})}
-      </div>
-      </>
-    
-      )}}
-  
 export default withAuth0(MyFavoriteBooks);
 
-
-{/* <Card key={i} style={{ width: '18rem' }}>
+{
+  /* <Card key={i} style={{ width: '18rem' }}>
   <Card.Body>
     <Card.Title>{item.title}</Card.Title>
     <Card.Text>
@@ -152,6 +148,18 @@ export default withAuth0(MyFavoriteBooks);
     {item.email}
     </Card.Text>
   </Card.Body>
-</Card> */}
+</Card> */
+}
 
+{
+  /* <li key={i}>
+<h4> {item.title}</h4>
+<p>{item.description}</p>
+<p>{item.email}</p>
+<Button variant="bottom" variant="danger" onClick={() => this.deleteBook(i)}>
+Delete 
+      </Button>
 
+</li>
+    )})} */
+}
